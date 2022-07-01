d3c08fdf9b7b70047a9256b65633def5250afeea
Md. Masud Rana <mr.hasib45@gmail.com>
2022-07-01 21:55:38 +0600

    add billing page

---
+++ src/components/BillingPage/BillingPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const BillingPage = () => {
+  return (
+    <div>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Billing ID</th>
+            <th scope="col">Full Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Paid Amount</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BillingPage;
